fc94f42ec90406c27552889adca66bc6cf26acb6
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-03-14 10:28:55 -0400

    Open and close edit button modal

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,19 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+/* -----------------------------------------------------------------------*/
+/*                                 Elements                               */
+/* -----------------------------------------------------------------------*/
+
+const profileEditBtn = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditBtn.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+const modalCloseBtn = document.querySelector("#modal-close-btn");
+
+modalCloseBtn.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
