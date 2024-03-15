f480d236d28e1313cb2926d9d9b7f66cb19b0dee
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-03-15 11:18:48 -0400

    linked photos and alt text

--- scripts/index.js
+++ scripts/index.js
@@ -39,6 +39,9 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 /*-------------------------------------------------------------*/
 /*-----------------------Functions-----------------------------*/
@@ -72,3 +75,20 @@ profileEditBtn.addEventListener("click", () => {
 profileModalCloseBtn.addEventListener("click", closePopup);
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  //clone the template element with all its content and store it in a cardElement variable
+  const cardElement = cardTemplate.cloneNode(true);
+  //access the card title and image and store them in variables
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  //set the path to the image to the link field of the object
+  cardImageEl.src = cardData.link;
+  //set the image alt text to the name field of the object
+  cardImageEl.alt = cardData.name;
+  //set the card title to the name field of the object, too
+  cardTitleEl.textContent = cardData.name;
+  //return the ready HTML element with the filled-in data
+  //return cardElement;
+  cardListEl.append(cardElement);
+});
