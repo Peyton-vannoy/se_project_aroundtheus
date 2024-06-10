4a759dc6890914bc4692ee7c084adf75bbf69e35
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-06-10 11:31:33 -0400

    refactor

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,3 +1,26 @@
-<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M22 9.77778H12.2222V0H9.77778V9.77778H0V12.2222H9.77778V22H12.2222V12.2222H22V9.77778Z" fill="white"/>
-</svg>
+class UserInfo {
+  constructor({ nameElement, aboutElement, avatarElement }) {
+    this._nameElement = document.querySelector(nameElement);
+    this._aboutElement = document.querySelector(aboutElement);
+    this._avatarElement = document.querySelector(avatarElement);
+  }
+
+  getUserInfo() {
+    this._userInfo = {
+      name: this._nameElement.textContent,
+      about: this._aboutElement.textContent,
+    };
+    return this._userInfo;
+  }
+
+  setUserInfo({ name, about }) {
+    this._nameElement.textContent = name;
+    this._aboutElement.textContent = about;
+  }
+
+  setUserAvatar({ avatar }) {
+    this._avatarElement.src = avatar;
+  }
+}
+
+export default UserInfo;
